6b00b755e6a8e67b194da31d2c036ebbebab1ae7
paulinabrzeczek <paulina.brzeczek01@gmail.com>
2024-11-12 16:49:40 +0100

    skroty

--- Źródło.cpp
+++ Źródło.cpp
@@ -1,96 +1,181 @@
 #include <windows.h>
 #include <commctrl.h>
+#include <commdlg.h>
+#include <vector>
 #include "resource.h"
 
+#pragma comment(lib, "Comctl32.lib")
+
+HFONT selFont = NULL;
 HWND hEdit;
 HWND hStatusBar;
-HINSTANCE hInst;
 
-// Funkcja zwrotna dla okna dialogowego "O Autorze"
-INT_PTR CALLBACK AboutDlgProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+// Funkcja do zmiany czcionki
+void ChangeFont(HWND hwnd) {
+    LOGFONT lf;
+    CHOOSEFONT cfnt;
+    ZeroMemory(&lf, sizeof(lf));
+    ZeroMemory(&cfnt, sizeof(cfnt));
+
+    // Pobranie aktualnej czcionki
+    selFont = (HFONT)SendMessage(hEdit, WM_GETFONT, 0, 0);
+    if (selFont == NULL)
+        selFont = (HFONT)GetStockObject(SYSTEM_FONT);
+
+    GetObject(selFont, sizeof(LOGFONT), &lf);
+
+    cfnt.lStructSize = sizeof(cfnt);
+    cfnt.hwndOwner = hwnd;
+    cfnt.lpLogFont = &lf;
+    cfnt.Flags = CF_EFFECTS | CF_SCREENFONTS | CF_INITTOLOGFONTSTRUCT;
+
+    // Wyœwietlenie okna dialogowego wyboru czcionki
+    if (ChooseFont(&cfnt)) {
+        HFONT newFont = CreateFontIndirect(&lf);
+        SendMessage(hEdit, WM_SETFONT, (WPARAM)newFont, TRUE);
+        selFont = newFont;
+    }
+}
+
+// Funkcja odczytu pliku do pola edycji
+BOOL ReadFileToEdit(HWND hEdit, LPCSTR fileName) {
+    BOOL success = FALSE;
+    HANDLE hFile = CreateFile(fileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+    if (hFile != INVALID_HANDLE_VALUE) {
+        DWORD fileSize = GetFileSize(hFile, NULL);
+        if (fileSize != INVALID_FILE_SIZE) {
+            std::vector<CHAR> buf(fileSize + 1);
+            DWORD dwRead;
+            if (ReadFile(hFile, buf.data(), fileSize, &dwRead, NULL)) {
+                buf[fileSize] = 0;
+                SetWindowText(hEdit, buf.data());
+                success = TRUE;
+            }
+        }
+        CloseHandle(hFile);
+    }
+    return success;
+}
+
+// Funkcja zapisu pola edycji do pliku
+BOOL WriteEditToFile(HWND hEdit, LPCSTR fileName) {
+    BOOL success = FALSE;
+    HANDLE hFile = CreateFile(fileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile != INVALID_HANDLE_VALUE) {
+        DWORD textLength = GetWindowTextLength(hEdit);
+        if (textLength > 0) {
+            std::vector<CHAR> buf(textLength + 1);
+            GetWindowText(hEdit, buf.data(), textLength + 1);
+            DWORD dwWrite;
+            if (WriteFile(hFile, buf.data(), textLength, &dwWrite, NULL)) {
+                success = TRUE;
+            }
+        }
+        CloseHandle(hFile);
+    }
+    return success;
+}
+
+// Funkcja okna dialogowego "O Autorze"
+INT_PTR CALLBACK AboutDlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
     case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
+        return TRUE;
     case WM_COMMAND:
         if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
-            EndDialog(hwndDlg, (INT_PTR)TRUE);
-            return (INT_PTR)TRUE;
+            EndDialog(hDlg, LOWORD(wParam));
+            return TRUE;
         }
         break;
     }
-    return (INT_PTR)FALSE;
+    return FALSE;
 }
 
-// Funkcja obs³ugi okna g³ównego
+// Funkcja obs³ugi komunikatów
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
     case WM_CREATE: {
-        // Tworzenie pola edycyjnego
-        hEdit = CreateWindowEx(0, TEXT("EDIT"), NULL,
-            WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE | ES_AUTOVSCROLL | ES_AUTOHSCROLL,
-            0, 0, CW_USEDEFAULT, CW_USEDEFAULT,
-            hwnd, (HMENU)1, hInst, NULL);
-
-        // Tworzenie paska stanu
-        INITCOMMONCONTROLSEX icc;
-        icc.dwSize = sizeof(INITCOMMONCONTROLSEX);
-        icc.dwICC = ICC_BAR_CLASSES;
-        InitCommonControlsEx(&icc);
-
-        hStatusBar = CreateWindowEx(0, STATUSCLASSNAME, NULL,
-            WS_CHILD | WS_VISIBLE | SBARS_SIZEGRIP,
-            0, 0, 0, 0,
-            hwnd, (HMENU)ID_STATUSBAR, hInst, NULL);
-
-        SendMessage(hStatusBar, SB_SETTEXT, 0, (LPARAM)TEXT("Aplikacja uruchomiona"));
+        InitCommonControls();  // Dodaj to, aby upewniæ siê, ¿e kontrolki s¹ za³adowane.
+        hEdit = CreateWindowEx(0, "EDIT", "", WS_CHILD | WS_VISIBLE | WS_VSCROLL | ES_MULTILINE | ES_AUTOVSCROLL,
+            0, 0, 800, 500, hwnd, (HMENU)1, GetModuleHandle(NULL), NULL);
+        hStatusBar = CreateWindowEx(0, STATUSCLASSNAME, NULL, WS_CHILD | WS_VISIBLE | SBARS_SIZEGRIP,
+            0, 0, 0, 0, hwnd, (HMENU)2, GetModuleHandle(NULL), NULL);
+        SendMessage(hStatusBar, SB_SETTEXT, 0, (LPARAM)"Gotowy.");
+        break;
     }
-                  break;
 
     case WM_SIZE: {
         RECT rcClient;
         GetClientRect(hwnd, &rcClient);
-
-        // Dopasowanie pola edycyjnego do rozmiaru okna
         SetWindowPos(hEdit, NULL, 0, 0, rcClient.right, rcClient.bottom - 20, SWP_NOZORDER);
-
-        // Dopasowanie paska stanu
         SendMessage(hStatusBar, WM_SIZE, 0, 0);
+        break;
     }
-                break;
 
-    case WM_COMMAND:
+    case WM_COMMAND: {
         switch (LOWORD(wParam)) {
-        case ID_PLIK_OTWORZ:
-            MessageBox(hwnd, TEXT("Otwórz wybrano"), TEXT("Informacja"), MB_OK);
+        case ID_PLIK_OTWORZ: {
+            char fileName[MAX_PATH] = "";
+            OPENFILENAME ofn = {};
+            ofn.lStructSize = sizeof(ofn);
+            ofn.hwndOwner = hwnd;
+            ofn.lpstrFile = fileName;
+            ofn.nMaxFile = MAX_PATH;
+            ofn.lpstrFilter = "Text Files\0*.TXT\0All Files\0*.*\0";
+            ofn.Flags = OFN_FILEMUSTEXIST;
+            if (GetOpenFileName(&ofn)) {
+                if (ReadFileToEdit(hEdit, fileName)) {
+                    SendMessage(hStatusBar, SB_SETTEXT, 0, (LPARAM)"Plik otwarty pomyœlnie.");
+                }
+                else {
+                    SendMessage(hStatusBar, SB_SETTEXT, 0, (LPARAM)"B³¹d podczas otwierania pliku.");
+                }
+            }
             break;
+        }
 
-        case ID_PLIK_ZAPISZ:
-            MessageBox(hwnd, TEXT("Zapisz wybrano"), TEXT("Informacja"), MB_OK);
-            SendMessage(hStatusBar, SB_SETTEXT, 0, (LPARAM)TEXT("Zapisano zawartoœæ"));
+        case ID_PLIK_ZAPISZ: {
+            char fileName[MAX_PATH] = "";
+            OPENFILENAME ofn = {};
+            ofn.lStructSize = sizeof(ofn);
+            ofn.hwndOwner = hwnd;
+            ofn.lpstrFile = fileName;
+            ofn.nMaxFile = MAX_PATH;
+            ofn.lpstrFilter = "Text Files\0*.TXT\0All Files\0*.*\0";
+            ofn.Flags = OFN_OVERWRITEPROMPT;
+            if (GetSaveFileName(&ofn)) {
+                if (WriteEditToFile(hEdit, fileName)) {
+                    SendMessage(hStatusBar, SB_SETTEXT, 0, (LPARAM)"Plik zapisany pomyœlnie.");
+                }
+                else {
+                    SendMessage(hStatusBar, SB_SETTEXT, 0, (LPARAM)"B³¹d podczas zapisywania pliku.");
+                }
+            }
             break;
-
-        case ID_PLIK_ZAKONCZ:
-            DestroyWindow(hwnd);
-            break;
+        }
 
         case ID_EDYCJA_ZAZNACZWSZYSTKO:
             SendMessage(hEdit, EM_SETSEL, 0, -1);
             break;
 
         case ID_EDYCJA_WYCZYSC:
-            SetWindowText(hEdit, TEXT(""));
+            SetWindowText(hEdit, "");
             break;
 
         case ID_EDYCJA_ZMIENCZCIONKE:
-            MessageBox(hwnd, TEXT("Zmiana czcionki niezaimplementowana"), TEXT("Informacja"), MB_OK);
+            ChangeFont(hwnd);
             break;
 
         case ID_POMOC_OAUTORZE:
-            DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), hwnd, AboutDlgProc);
+            DialogBox(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG1), hwnd, AboutDlgProc);
+            break;
+
+        case ID_PLIK_ZAKONCZ:
+            PostQuitMessage(0);
             break;
         }
         break;
+    }
 
     case WM_CLOSE:
         DestroyWindow(hwnd);
@@ -99,35 +184,31 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
-
-    default:
-        return DefWindowProc(hwnd, message, wParam, lParam);
     }
-    return 0;
+    return DefWindowProc(hwnd, message, wParam, lParam);
 }
 
-// Funkcja WinMain
-int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow) {
-    hInst = hInstance;
+// Funkcja g³ówna
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "Notatnik";
 
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_HREDRAW | CS_VREDRAW, WndProc,
-        0, 0, hInstance, NULL, LoadCursor(NULL, IDC_ARROW),
-        (HBRUSH)(COLOR_WINDOW + 1), NULL, TEXT("Notatnik"), NULL };
+    RegisterClass(&wc);
 
-    RegisterClassEx(&wc);
-
-    HWND hwnd = CreateWindowEx(0, TEXT("Notatnik"), TEXT("Notatnik"),
-        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+    HWND hwnd = CreateWindow("Notatnik", "Notatnik", WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
         NULL, LoadMenu(hInstance, MAKEINTRESOURCE(IDR_MENU2)), hInstance, NULL);
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
     MSG msg;
+
     HACCEL hAccel = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDR_ACCELERATOR1));
 
     while (GetMessage(&msg, NULL, 0, 0)) {
-        if (!TranslateAccelerator(hwnd, hAccel, &msg)) {
+        if (!TranslateAccelerator(hwnd, hAccel, &msg)) {  // Dodajemy tê linijkê
             TranslateMessage(&msg);
             DispatchMessage(&msg);
         }
